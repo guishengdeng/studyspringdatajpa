6ec69542e4ec366ef424c2816d1eabfae3d235a7
guisheng.deng <1582340648@qq.com>
2017-04-05 10:27:44 +0800

    资源列表的js文件

--- src/main/webapp/jquery/js/resource.js
+++ src/main/webapp/jquery/js/resource.js
@@ -1,5 +1,6 @@
 $(function(){
     updateOrAddSubmit();
+    add();
 });
 /*添加或者修改的form表单提交事件*/
 function  updateOrAddSubmit(){
@@ -33,7 +34,17 @@ function editInfo(id) {
             $('#resourcesList').empty();
             $("#resource_id").val(data.resource.id);
             $("#resource_resourcename").val(data.resource.resourcename);
-            $('#resource_linkedaddress').val(data.resource.linkedaddress);
+            $('#resource_linkedaddress').val('').attr("readonly","readonly");
+            $('#menuItem').empty();
+            if(data.currentMenuItem!=null){
+                $("#menuItem").append('<input type="checkbox" name="menuitem_id" value="' + data.currentMenuItem.menuitem_id + '" checked="checked" />' + data.currentMenuItem.name);
+            }
+            if(data.menuItems!=null){
+                for(var index=0;index<data.menuItems.length;index++){
+                    var menuitem=data.menuItems[index];
+                    $("#menuItem").append('<input type="checkbox" name="menuitem_id" value="' + menuitem.menuitem_id + '" />' + menuitem.name);
+                }
+            }
             $("#resource_description").val(data.resource.description);
         }
 
@@ -56,5 +67,25 @@ function deleteInfo(id) {
         success: function (data) {
             window.location.href = "../resource/resourcelist.action";
         }
+    });
+}
+function add(){
+    $('#addResource').bind("click",function(){
+        $.ajax({
+            type: "POST",
+            url: '../resource/add.action',
+            dataType:"json",
+            async: false, // 是否异步加载数据， 为false时只有加载完成后才继续其他操作，类似于同步锁
+            success: function (data) {
+                if(data.menuItems!=null){
+                    for(var index=0;index<data.menuItems.length;index++){
+                        var menuitem=data.menuItems[index];
+                        $("#menuItem").append('<input type="checkbox" name="menuitem_id" value="' + menuitem.menuitem_id + '" />' + menuitem.name);
+                    }
+                }
+            },error: function (request) {
+                alert("Connection error");
+            }
+        });
     });
 }
