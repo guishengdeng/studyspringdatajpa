97896da0c75c246e537a0890be462dee14b7593d
guisheng.deng <1582340648@qq.com>
2017-04-05 09:55:14 +0800

    用户登陆验证

--- src/main/java/com/spring/jpa/security/CustomerService.java
+++ src/main/java/com/spring/jpa/security/CustomerService.java
@@ -40,37 +40,10 @@ public class CustomerService implements UserDetailsService{
          if(user==null)
              throw new UsernameNotFoundException("The user name " + username
                      + " can not be found!");
-        //查询用户表时，会加载其关联的role角色表（role类）
-         Set<Role> roles=user.getRoles();
-         Set<GrantedAuthority> grantedAuthoritySet=new HashSet<GrantedAuthority>();
-         //增加access中配置的权限，实际上这里就是让所有登陆用户都具备该权限，
-         //而真正的资源权限验证留给AccessDecisionManager来决定
-         if(roles!=null){
-             for(Role role:roles){
-                 //grantedAuthorityList.add(new GrantAuthorityImpl(role.getName()));
-                 //Role currentRole=roleService.getRoleByName(role.getName());
-                 Set<Resource> set=role.getResources();
-                 if(set!=null){
-                     for(Resource resource:set){
-                         //这个地方到时候还要改，因为到时候在资源表中，用户在做修改时，可能会对资源进行;的增添
-                         String [] splitStr=resource.getResourcename().split(";");
-                         if(splitStr.length>=2){
-                             for(String item:splitStr){
-                                 grantedAuthoritySet.add(new GrantAuthorityImpl(item));
-                             }
-                         }else{
-                             grantedAuthoritySet.add(new GrantAuthorityImpl(resource.getResourcename()));
-                         }
-                     }
-                 }
-             }
-         }
-
-        //验证用户名和密码是否正确，以及是否权限正确
-        LoginUserValidate userValidate=new LoginUserValidate(user.getUsername(),user.getPassword(),
-                grantedAuthoritySet,true,true,
-                true,true,roles);
-         return userValidate;
+        Set<Role> roles=user.getRoles();
+        //备注：在po类User里面已经做了权限验证
+        user.setRoleDirectory(roles);
+         return user;
 
     }
 }
