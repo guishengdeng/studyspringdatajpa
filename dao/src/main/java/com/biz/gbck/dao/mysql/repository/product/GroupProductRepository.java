265da1e257f91d5bed5b58b7acc343fee2b824b4
david.liu <recalleverything@163.com>
2017-04-20 15:32:35 +0800

    move the old source code to functional area

--- dao/src/main/java/com/biz/gbck/dao/mysql/repository/product/GroupProductRepository.java
+++ dao/src/main/java/com/biz/gbck/dao/mysql/repository/product/GroupProductRepository.java
@@ -1,6 +1,6 @@
-package com.biz.gbck.dao.mysql.repository.product;
+package com.biz.gbck.dao.mysql.repository.bbc.product;
 
-import com.biz.gbck.dao.mysql.po.product.bbc.GroupProduct;
+import com.biz.gbck.dao.mysql.po.product.GroupProduct;
 import com.biz.support.jpa.repository.CommonJpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
