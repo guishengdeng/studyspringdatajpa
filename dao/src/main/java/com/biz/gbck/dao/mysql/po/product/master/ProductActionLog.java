3d00dad51a83a556806ec324f79630f5901dd5f8
david.liu <recalleverything@163.com>
2017-04-21 10:51:14 +0800

    organize the jpa entities bellow package dao.mysql.po.product

---
+++ dao/src/main/java/com/biz/gbck/dao/mysql/po/product/master/ProductActionLog.java
@@ -0,0 +1,7 @@
+package com.biz.gbck.dao.mysql.po.product.master;
+
+/**
+ * Created by david-liu on 2017/04/21 10:38.
+ */
+public class ProductActionLog {
+}
---
+++ dao/src/main/java/com/biz/gbck/enums/CommonActionEnum.java
@@ -0,0 +1,7 @@
+package com.biz.gbck.enums;
+
+/**
+ * Created by david-liu on 2017/04/21 10:41.
+ */
+public enum CommonActionEnum {
+}
