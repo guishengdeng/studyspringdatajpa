787b66106cb769646b3bb42405acbaa43f5fc77e
david.liu <recalleverything@163.com>
2017-04-21 09:00:00 +0800

    update image upload reference to track original image suffix

--- manage/src/main/java/com/biz/manage/controller/file/UploadController.java
+++ manage/src/main/java/com/biz/manage/controller/file/UploadController.java
@@ -8,29 +8,28 @@ import com.biz.core.ali.oss.util.OssUtil;
 import com.biz.core.exceptions.BizSystemException;
 import com.biz.core.util.ImageUtil;
 import com.biz.support.web.handler.JSONResult;
+import java.io.IOException;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.UUID;
-
 @Controller
 @RequestMapping("/upload")
 public class UploadController {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private static final String UPLOAD_STREAM_PARAM = "base64stream"; //base64方式上传
+    private static final String UPLOAD_STREAM_PARAM = "base64stream"; //base64方式上传
 
-	private static final String UPLOAD_NAME_PARAM = "key"; //文件方式上传
+    private static final String UPLOAD_NAME_PARAM = "key"; //文件方式上传
 
     private static final String STATUS = "status";
 
@@ -47,61 +46,63 @@ public class UploadController {
     private static final String PREVIEW_PARAM = "image_name";
 
     @Autowired
-	private OssConfig config;
+    private OssConfig config;
 
-	@Autowired
-	private OSSClient ossClient;
+    @Autowired
+    private OSSClient ossClient;
 
-	@RequestMapping(value = "fileUpload", method = RequestMethod.POST)
-	public JSONResult fileUpload(MultipartFile file, HttpServletRequest request) {
-		String key = request.getParameter(UPLOAD_NAME_PARAM);
-		try {
-			PutObjectRequest req = new PutObjectRequest(config.getBucketName(), key, file.getInputStream
-					());
-			OssUtil.putObject(ossClient, req);
-			return new JSONResult(0,"上传成功");
-		} catch (Exception e) {
-			logger.error("上传失败", e);
-			return new JSONResult(1,"上传失败");
-		}
-	}
+    @RequestMapping(value = "fileUpload", method = RequestMethod.POST)
+    public JSONResult fileUpload(MultipartFile file, HttpServletRequest request) {
+        String key = request.getParameter(UPLOAD_NAME_PARAM);
+        try {
+            PutObjectRequest req = new PutObjectRequest(config.getBucketName(), key, file.getInputStream
+                    ());
+            OssUtil.putObject(ossClient, req);
+            return new JSONResult(0, "上传成功");
+        } catch (Exception e) {
+            logger.error("上传失败", e);
+            return new JSONResult(1, "上传失败");
+        }
+    }
 
-	@RequestMapping(value = "streamUpload", method = RequestMethod.POST)
-	@ResponseBody
-	public JSONResult upload(HttpServletRequest request) {
-		String base64stream = request.getParameter(UPLOAD_STREAM_PARAM);
-		String key = UUID.randomUUID().toString();
-		try {
-			PutObjectRequest req = new PutObjectRequest(config.getBucketName(), key, ImageUtil.base64stream2image
-					(base64stream));
-			OssUtil.putObject(ossClient, req);
-			String imageUri = OssUtil.getOssResourceUri(config.getBucketName(), config.getRemoteEndpoint(), key);
-			return new JSONResult(imageUri);
-		} catch (Exception e) {
-			logger.error("上传失败", e);
-			return new JSONResult(1, "上传失败");
-		}
-	}
+    @PostMapping(value = "streamUpload")
+    @ResponseBody
+    public JSONResult upload(HttpServletRequest request) {
+        String base64stream = request.getParameter(UPLOAD_STREAM_PARAM);
+        try {
+            ImageUtil.Base64stream2imageTransResult imageTransResult = ImageUtil.base64stream2image(base64stream);
+            String key = String.format(UUID.randomUUID().toString(), imageTransResult.imagePattern);
+            PutObjectRequest req = new PutObjectRequest(config.getBucketName(), key, imageTransResult.stream);
+            OssUtil.putObject(ossClient, req);
+            String imageUri = OssUtil.getOssResourceUri(config.getBucketName(), config.getRemoteEndpoint(), key);
+            return new JSONResult(imageUri);
+        } catch (Exception e) {
+            logger.error("上传失败", e);
+            return new JSONResult(1, "上传失败");
+        }
+    }
 
-	@RequestMapping(value = "preview", method = RequestMethod.POST)
-	@ResponseBody
-	public JSONObject sourceUri(HttpServletRequest request) {
-		String imageName = request.getParameter(PREVIEW_PARAM);
-		JSONObject json = new JSONObject();
-		json.put(URI_FLAG, OssUtil.getOssResourceUri(config.getBucketName(), config.getRemoteEndpoint(), imageName));
-		return json;
-	}
+    @RequestMapping(value = "preview", method = RequestMethod.POST)
+    @ResponseBody
+    public JSONObject sourceUri(HttpServletRequest request) {
+        String imageName = request.getParameter(PREVIEW_PARAM);
+        JSONObject json = new JSONObject();
+        json.put(URI_FLAG, OssUtil.getOssResourceUri(config.getBucketName(), config.getRemoteEndpoint(), imageName));
+        return json;
+    }
 
     @RequestMapping(value = "uploadTest", method = RequestMethod.POST)
     @ResponseBody
     public JSONObject uploadTest(HttpServletRequest request) {
         String base64stream = request.getParameter(UPLOAD_STREAM_PARAM);
-        logger.debug("base64--------"+base64stream);
-        String key = UUID.randomUUID().toString();
+        logger.debug("base64--------" + base64stream);
         PutObjectRequest req = null;
         JSONObject jsonObject = new JSONObject();
+        String key = null;
         try {
-            req = new PutObjectRequest(config.getBucketName(), key, ImageUtil.base64stream2image(base64stream));
+            ImageUtil.Base64stream2imageTransResult imageTransResult = ImageUtil.base64stream2image(base64stream);
+            key = String.format(UUID.randomUUID().toString(), imageTransResult.imagePattern);
+            req = new PutObjectRequest(config.getBucketName(), key, imageTransResult.stream);
         } catch (IOException e) {
             logger.error("转换base64图片编码出错", e);
             jsonObject.put(STATUS, ERROR_FLAG);
