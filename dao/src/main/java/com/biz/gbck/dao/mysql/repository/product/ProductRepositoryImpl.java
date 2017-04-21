a7073a5d3e6719b172c90044d99723678431efe5
david.liu <recalleverything@163.com>
2017-04-21 11:30:44 +0800

    append product spring data jpa repository reference

---
+++ dao/src/main/java/com/biz/gbck/dao/mysql/repository/product/ProductRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.biz.gbck.dao.mysql.repository.product;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 商品RepositoryImpl
+ *
+ * Created by david-liu on 2017/04/21 11:26.
+ */
+@Repository
+public class ProductRepositoryImpl implements ProductDao {
+}
