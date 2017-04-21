b6a4d6fac7ab99f7b8f334dab316647dfcb8989f
david.liu <recalleverything@163.com>
2017-04-21 11:30:21 +0800

    append price group spring data jpa repository resources

---
+++ dao/src/main/java/com/biz/gbck/dao/mysql/repository/priceGroup/PriceGroupDao.java
@@ -0,0 +1,9 @@
+package com.biz.gbck.dao.mysql.repository.priceGroup;
+
+/**
+ * 商品价格组Dao
+ *
+ * Created by david-liu on 2017/04/21 11:27.
+ */
+public interface PriceGroupDao {
+}
---
+++ dao/src/main/java/com/biz/gbck/dao/mysql/repository/priceGroup/PriceGroupRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.biz.gbck.dao.mysql.repository.priceGroup;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 商品价格组RepositoryImpl
+ *
+ * Created by david-liu on 2017/04/21 11:29.
+ */
+@Repository
+public class PriceGroupRepositoryImpl implements PriceGroupDao {
+}
