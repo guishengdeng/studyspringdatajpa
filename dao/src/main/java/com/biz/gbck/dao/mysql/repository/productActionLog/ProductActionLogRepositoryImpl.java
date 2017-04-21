9ad91b3a0f1626f5061e491335c72ae162e8dc2a
david.liu <recalleverything@163.com>
2017-04-21 11:34:38 +0800

    append product action log spring data jpa reference resources

---
+++ dao/src/main/java/com/biz/gbck/dao/mysql/repository/productActionLog/ProductActionLogRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.biz.gbck.dao.mysql.repository.productActionLog;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 商品维护记录RepositoryImpl
+ *
+ * Created by david-liu on 2017/04/21 11:34.
+ */
+@Repository
+public class ProductActionLogRepositoryImpl implements ProductActionLogDao {
+}
