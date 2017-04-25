237006d255aa214896ae4f54b1246dd0f7c35786
lei <helei_24@163.com>
2017-04-25 14:34:20 +0800

    add cart redis and ros

---
+++ dao/src/main/java/com/biz/gbck/dao/redis/ro/cart/ShopCartItemRo.java
@@ -0,0 +1,93 @@
+package com.biz.gbck.dao.redis.ro.cart;
+
+import com.biz.redis.annotation.FieldSortedSet;
+import com.biz.redis.annotation.Ro;
+import com.biz.redis.bean.BaseRedisObject;
+
+/**
+ * 购物车ro对象
+ *
+ * @author lei
+ * @date 2017/04/24
+ */
+@Ro(key = "cart")
+public class ShopCartItemRo extends BaseRedisObject<String> {
+
+    private String id;
+
+    /**
+     * 商品Id
+     */
+    private Long productId;
+
+    /**
+     * 商品编码
+     */
+    private String productCode;
+
+    /**
+     * 是否选中
+     */
+    private boolean selected;
+
+    /**
+     * 用户Id
+     */
+    @FieldSortedSet(key = "userId", score = "updateTimestamp")
+    private Long userId;
+
+    /**
+     * 数量
+     */
+    private Integer quantity;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
---
+++ rest/src/main/java/com/biz/rest/controller/order/OrderController.java
@@ -0,0 +1,19 @@
+package com.biz.rest.controller.order;
+
+import com.biz.rest.controller.BaseRestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单controller
+ *
+ * @author lei
+ * @date 2017年04月25日
+ * @reviewer
+ * @see
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController extends BaseRestController {
+
+}
