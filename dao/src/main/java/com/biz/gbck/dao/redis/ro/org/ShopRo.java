4a35fcfc15dfc38455b61fcb3d7a2a1ed53ac5b0
liubin <liubin518@me.com>
2017-04-25 18:05:30 +0800

    add ro

---
+++ dao/src/main/java/com/biz/gbck/dao/redis/ro/org/ShopRo.java
@@ -0,0 +1,547 @@
+package com.biz.gbck.dao.redis.ro.org;
+
+import com.biz.redis.bean.BaseRedisObject;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+/**
+ * 店铺
+ * Created by defei on 3/16/16.
+ */
+public class ShopRo extends BaseRedisObject<Long> {
+
+
+    private Long id;
+
+    /**
+     * 店铺名称
+     */
+    private String name;
+
+    /**
+     * 法人名字
+     */
+    private String corporateName;
+
+    /**
+     * 店铺头像
+     */
+    private String avatar;
+
+    /**
+     * 店铺成员
+     */
+    private String users;
+
+    /**
+     * 店铺类型
+     */
+    private Long shopTypeId;
+
+    /**
+     * 店铺手机
+     */
+    private String mobile;
+
+    /**
+     * 店铺电话
+     */
+    private String tel;
+
+    /**
+     * 绑定门店价格门店id
+     */
+    private String depotId;
+
+    
+    /**
+     * 配送门店Id
+     */
+    private String deliveryDepotId;
+    
+    
+    /**
+     * 店铺经度
+     */
+    private BigDecimal longitude;
+
+    /**
+     * 店铺纬度
+     */
+    private BigDecimal latitude;
+
+    /**
+     * 收货人姓名
+     */
+    private String deliveryName;
+
+    /**
+     * 收货人电话
+     */
+    private String deliveryMobile;
+
+    /**
+     * 店铺收货地址
+     */
+    private String deliveryAddress;
+
+    /**
+     * 店铺地址
+     */
+    private String shopAddress;
+
+    /**
+     * 省
+     */
+    private Long provinceId;
+
+    /**
+     * 市
+     */
+    private Long cityId;
+
+    /**
+     * 区
+     */
+    private Long districtId;
+
+    /**
+     * 营业执照ID
+     */
+    private String businessLicenceId;
+
+    /**
+     * 营业执照名称
+     */
+    private String businessLicenceName;
+
+    /**
+     * 营业执照
+     */
+    private String businessLicence;
+
+    /**
+     * 门头照片
+     */
+    private String shopPhoto;
+
+    /**
+     * 酒类流通许可证ID
+     */
+    private String liquorSellLicenceId;
+
+    /**
+     * 酒类流通许可证
+     */
+    private String liquorSellLicence;
+
+    /**
+     * 法人身份证Id
+     */
+    private String corporateId;
+
+    /**
+     * 法人身份证
+     */
+    private String corporateIdPhoto;
+
+    /**
+     * 创建时间
+     */
+    private Timestamp createTime;
+
+    /**
+     * 智选价格标签
+     */
+    private String priceTags;
+
+    /**
+     * 智选分类标签
+     */
+    private String businessTags;
+
+    /**
+     * 销售区域
+     */
+    private String saleAreas;
+
+    /**
+     * 邀请者
+     */
+    private String inviterCode;
+
+    /**
+     * 商户详情审核状态
+     */
+    private Integer detailAuditStatus;
+
+    /**
+     * 资质审核状态
+     */
+    private Integer qualificationAuditStatus;
+
+    /**
+     * 店铺状态
+     */
+    private Integer status;
+
+    /**
+     * 支持的 付款方式
+     */
+    private String supportPaymentIds;
+
+    /**
+     * 禁用的付款方式
+     */
+    private String disabledPaymentIds;
+
+    /**
+     * 支付密码
+     */
+    private String paymentPassword;
+
+
+    private Long parentId;
+
+    private Integer channel;
+
+    private Long channelUserId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCorporateName() {
+        return corporateName;
+    }
+
+    public void setCorporateName(String corporateName) {
+        this.corporateName = corporateName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUsers() {
+        return users;
+    }
+
+    public void setUsers(String users) {
+        this.users = users;
+    }
+
+    public Long getShopTypeId() {
+        return shopTypeId;
+    }
+
+    public void setShopTypeId(Long shopTypeId) {
+        this.shopTypeId = shopTypeId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    /**
+     * {@linkplain ShopRo#depotId}
+     */
+    public String getDepotId() {
+        return depotId;
+    }
+
+    public void setDepotId(String depotId) {
+        this.depotId = depotId;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDeliveryName() {
+        return deliveryName;
+    }
+
+    public void setDeliveryName(String deliveryName) {
+        this.deliveryName = deliveryName;
+    }
+
+    public String getDeliveryMobile() {
+        return deliveryMobile;
+    }
+
+    public void setDeliveryMobile(String deliveryMobile) {
+        this.deliveryMobile = deliveryMobile;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getShopAddress() {
+        return shopAddress;
+    }
+
+    public void setShopAddress(String shopAddress) {
+        this.shopAddress = shopAddress;
+    }
+
+    public Long getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(Long provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
+
+    public Long getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(Long districtId) {
+        this.districtId = districtId;
+    }
+
+    public String getBusinessLicenceId() {
+        return businessLicenceId;
+    }
+
+    public void setBusinessLicenceId(String businessLicenceId) {
+        this.businessLicenceId = businessLicenceId;
+    }
+
+    public String getBusinessLicenceName() {
+        return businessLicenceName;
+    }
+
+    public void setBusinessLicenceName(String businessLicenceName) {
+        this.businessLicenceName = businessLicenceName;
+    }
+
+    public String getBusinessLicence() {
+        return businessLicence;
+    }
+
+    public void setBusinessLicence(String businessLicence) {
+        this.businessLicence = businessLicence;
+    }
+
+    public String getShopPhoto() {
+        return shopPhoto;
+    }
+
+    public void setShopPhoto(String shopPhoto) {
+        this.shopPhoto = shopPhoto;
+    }
+
+    public String getLiquorSellLicenceId() {
+        return liquorSellLicenceId;
+    }
+
+    public void setLiquorSellLicenceId(String liquorSellLicenceId) {
+        this.liquorSellLicenceId = liquorSellLicenceId;
+    }
+
+    public String getLiquorSellLicence() {
+        return liquorSellLicence;
+    }
+
+    public void setLiquorSellLicence(String liquorSellLicence) {
+        this.liquorSellLicence = liquorSellLicence;
+    }
+
+    public String getCorporateId() {
+        return corporateId;
+    }
+
+    public void setCorporateId(String corporateId) {
+        this.corporateId = corporateId;
+    }
+
+    public String getCorporateIdPhoto() {
+        return corporateIdPhoto;
+    }
+
+    public void setCorporateIdPhoto(String corporateIdPhoto) {
+        this.corporateIdPhoto = corporateIdPhoto;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getPriceTags() {
+        return priceTags;
+    }
+
+    public void setPriceTags(String priceTags) {
+        this.priceTags = priceTags;
+    }
+
+    public String getBusinessTags() {
+        return businessTags;
+    }
+
+    public void setBusinessTags(String businessTags) {
+        this.businessTags = businessTags;
+    }
+
+    public String getSaleAreas() {
+        return saleAreas;
+    }
+
+    public void setSaleAreas(String saleAreas) {
+        this.saleAreas = saleAreas;
+    }
+
+    public String getInviterCode() {
+        return inviterCode;
+    }
+
+    public void setInviterCode(String inviterCode) {
+        this.inviterCode = inviterCode;
+    }
+
+    public Integer getDetailAuditStatus() {
+        return detailAuditStatus;
+    }
+
+    public void setDetailAuditStatus(Integer detailAuditStatus) {
+        this.detailAuditStatus = detailAuditStatus;
+    }
+
+    public Integer getQualificationAuditStatus() {
+        return qualificationAuditStatus;
+    }
+
+    public void setQualificationAuditStatus(Integer qualificationAuditStatus) {
+        this.qualificationAuditStatus = qualificationAuditStatus;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    
+    public String getDeliveryDepotId() {
+		return deliveryDepotId;
+	}
+
+	public void setDeliveryDepotId(String deliveryDepotId) {
+		this.deliveryDepotId = deliveryDepotId;
+	}
+
+	public String getSupportPaymentIds() {
+		return supportPaymentIds;
+	}
+
+	public void setSupportPaymentIds(String supportPaymentIds) {
+		this.supportPaymentIds = supportPaymentIds;
+	}
+
+    public String getDisabledPaymentIds() {
+        return disabledPaymentIds;
+    }
+
+    public void setDisabledPaymentIds(String disabledPaymentIds) {
+        this.disabledPaymentIds = disabledPaymentIds;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public Integer getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Integer channel) {
+        this.channel = channel;
+    }
+
+    public Long getChannelUserId() {
+        return channelUserId;
+    }
+
+    public void setChannelUserId(Long channelUserId) {
+        this.channelUserId = channelUserId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String toString(){
+    	return ToStringBuilder.reflectionToString(this);
+    }
+
+    public String getPaymentPassword() {
+        return paymentPassword;
+    }
+
+    public void setPaymentPassword(String paymentPassword) {
+        this.paymentPassword = paymentPassword;
+    }
+
+}
---
+++ dao/src/main/java/com/biz/gbck/dao/redis/ro/org/ShopTypeRo.java
@@ -0,0 +1,87 @@
+package com.biz.gbck.dao.redis.ro.org;
+
+
+import com.biz.redis.bean.BaseRedisObject;
+
+/**
+ * Created by defei on 3/18/16.
+ */
+public class ShopTypeRo extends BaseRedisObject<Long> {
+
+
+    private Long id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 显示顺序
+     */
+    private Integer idx = 0;
+
+    /**
+     * 权重 (后台管理暂时不维护)
+     */
+    private Integer weight;
+
+    /**
+     * 状态(0：删除,1:正常)
+     */
+    private Integer status;
+
+    /**
+     * 备注
+     */
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Integer idx) {
+        this.idx = idx;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
