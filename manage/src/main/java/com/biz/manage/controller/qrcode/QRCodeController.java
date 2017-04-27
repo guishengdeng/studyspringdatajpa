878080f8b43362ead580a3b491c922d49daa2c9f
xing.sun <1234@qq.com>
2017-04-27 18:55:34 +0800

    分页

--- manage/src/main/java/com/biz/manage/controller/qrcode/QRCodeController.java
+++ manage/src/main/java/com/biz/manage/controller/qrcode/QRCodeController.java
@@ -1,19 +1,25 @@
 package com.biz.manage.controller.qrcode;
 
-import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.biz.gbck.dao.mysql.po.qrcode.QRCodePO;
-import com.biz.gbck.enums.CommonStatusEnum;
+import com.biz.gbck.dao.mysql.po.qrcode.QRCodeResumePO;
+import com.biz.gbck.vo.qrcode.QRCodeSearchVO;
 import com.biz.manage.controller.BaseController;
+import com.biz.service.qrcode.interfaces.QRCodeResumeService;
 import com.biz.service.qrcode.interfaces.QRCodeService;
 
 /**
@@ -24,14 +30,45 @@ import com.biz.service.qrcode.interfaces.QRCodeService;
 @Controller
 @RequestMapping("qrcode")
 @Secured("ROLE_QRCODE")
-public class QRCodeController extends BaseController{
+public class QRCodeController extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(QRCodeController.class);
+
     @Autowired
     private QRCodeService qrcodeService;
-    
+
+    @Autowired
+    private QRCodeResumeService qrcodeResumeService;
+
     @GetMapping("list")
     @PreAuthorize("hasAuthority('OPT_QRCODE_LIST')")
-    public ModelAndView list(@RequestParam(value = "status", required = false, defaultValue = "ENABLE") CommonStatusEnum status) {
-        List<QRCodePO> qrcodes = qrcodeService.listByStatus(status);
-        return new ModelAndView("qrcode/list", "qrcodes", qrcodes).addObject("enabled", status.isEnable());
+    public ModelAndView list(@ModelAttribute("reqVo") QRCodeSearchVO reqVo) {
+        Page<QRCodePO> qrcodes = qrcodeService.searchQRCode(reqVo);
+        ModelAndView mav=new ModelAndView("qrcode/list");
+        mav.addObject("qrcodes", qrcodes);
+        return mav;
+    }
+
+    @PostMapping("delete")
+    @PreAuthorize("hasAuthority('OPT_QRCODE_DELETE')")
+    @ResponseBody
+    public Boolean delete(@RequestParam("id") String id) {
+        try {
+            qrcodeService.remove(id);
+            return true;
+        } catch (Exception e) {
+            logger.info("Kill cat[{}] failed.", id, e);
+            return false;
+        }
+    }
+
+    /***** 二维码履历 **************************************************/
+    @GetMapping("resume/list")
+    @PreAuthorize("hasAuthority('OPT_QRCODE_RESUME_LIST')")
+    public ModelAndView listQRCodeResume(@ModelAttribute("reqVo") QRCodeSearchVO reqVo) {
+        logger.debug("进入  qrcode/resume/list 页面");
+        Page<QRCodeResumePO> qrcodes = qrcodeResumeService.searchQRCodeResume(reqVo);
+        ModelAndView mav=new ModelAndView("qrcode/resume/list");
+        mav.addObject("qrcodes", qrcodes);
+        return mav;
     }
 }
