72002c4e82e0041f93632df7d5931c03c63ddb05
dylan <dylan.hou@biz-united.com.cn>
2017-05-03 10:14:12 +0800

    update rest shopController

--- service/src/main/java/com/biz/service/org/ShopServiceImpl.java
+++ service/src/main/java/com/biz/service/org/ShopServiceImpl.java
@@ -1,6 +1,8 @@
 package com.biz.service.org;
 
+import com.biz.core.util.DateUtil;
 import com.biz.event.org.ShopDetailUpdateEvent;
+import com.biz.event.org.ShopQualificationUpdateEvent;
 import com.biz.gbck.common.bean.PageControl;
 import com.biz.gbck.common.exception.CommonException;
 import com.biz.gbck.common.exception.DepotnearbyExceptionFactory;
@@ -17,6 +19,7 @@ import com.biz.gbck.dao.mysql.repository.geo.CityRepository;
 import com.biz.gbck.dao.mysql.repository.geo.DistrictRepository;
 import com.biz.gbck.dao.mysql.repository.geo.ProvinceRepository;
 import com.biz.gbck.dao.mysql.repository.org.ShopDetailRepository;
+import com.biz.gbck.dao.mysql.repository.org.ShopQualificationRepository;
 import com.biz.gbck.dao.mysql.repository.org.ShopRepository;
 import com.biz.gbck.dao.mysql.repository.org.ShopTypeRepository;
 import com.biz.gbck.dao.redis.ro.org.ShopRo;
@@ -24,6 +27,7 @@ import com.biz.gbck.dao.redis.ro.org.ShopTypeRo;
 import com.biz.gbck.dao.redis.ro.org.UserRo;
 import com.biz.gbck.enums.user.AuditStatus;
 import com.biz.gbck.enums.user.ShopStatus;
+import com.biz.gbck.transform.org.ShopPoToShopDetailPo;
 import com.biz.gbck.vo.org.ShopExportVo;
 import com.biz.gbck.vo.org.ShopsInfoExportVo;
 import com.biz.gbck.vo.org.User20VIPVo;
@@ -36,6 +40,7 @@ import com.biz.service.CommonService;
 import com.biz.service.org.interfaces.ShopService;
 import com.biz.service.org.interfaces.UserService;
 import com.biz.vo.org.*;
+import org.codelogger.utils.BeanUtils;
 import org.codelogger.utils.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -80,6 +85,9 @@ public class ShopServiceImpl extends CommonService  implements ShopService {
 
     @Autowired
     private DistrictRepository districtRepository;
+
+    @Autowired
+    private ShopQualificationRepository shopQualificationRepository;
 
 
     @Override
@@ -185,7 +193,21 @@ public class ShopServiceImpl extends CommonService  implements ShopService {
 
     @Override
     public ShopDetailPo findLatestDetail(ShopDetailOrQualificationGetReqVo reqVo) throws CommonException {
-        return null;
+        UserRo user = userService.findUser(reqVo.getUserId());
+        validateShopOperateAuthority(user.getIsAdmin(), reqVo.getShopId(), user.getShopId());
+        ShopDetailPo latestShopDetailPo = CollectionUtils
+                .getFirstOrNull(shopDetailRepository.findByShopIdOrderByIdDesc(user.getShopId()));
+        if (latestShopDetailPo == null) {
+            latestShopDetailPo = new ShopDetailPo();
+            ShopPo shopPo = findShopPo(reqVo.getShopId());
+            latestShopDetailPo.setShop(shopPo);
+            latestShopDetailPo.setProvince(shopPo.getProvince());
+            latestShopDetailPo.setCity(shopPo.getCity());
+            latestShopDetailPo.setDistrict(shopPo.getDistrict());
+            latestShopDetailPo.setDeliveryAddress(shopPo.getDeliveryAddress());
+            latestShopDetailPo.setAuditStatus(shopPo.getDetailAuditStatus());
+        }
+        return latestShopDetailPo;
     }
 
     @Override
@@ -196,11 +218,42 @@ public class ShopServiceImpl extends CommonService  implements ShopService {
     @Override
     public void updateQualification(ShopUpdateQualificationReqVo reqVo) throws CommonException {
 
+        UserRo user = userService.findUser(reqVo.getUserId());
+        validateShopOperateAuthority(user.getIsAdmin(), reqVo.getShopId(), user.getShopId());
+
+        List<ShopQualificationPo> shopQualificationsOfWaitForAudit = shopQualificationRepository
+                .findByShopIdAndAuditStatusInOrderByIdDesc(reqVo.getShopId(),
+                        newArrayList(AuditStatus.WAIT_FOR_AUDIT.getValue(),
+                                AuditStatus.NORMAL_AND_HAS_NEW_UPDATE_WAIT_FOR_AUDIT.getValue()));
+
+        ShopQualificationPo shopQualificationPo =
+                CollectionUtils.getFirstOrNull(shopQualificationsOfWaitForAudit);
+        if (shopQualificationPo == null) {
+            shopQualificationPo = new ShopQualificationPo();
+            ShopPo shopPo = shopRepository.findOne(reqVo.getShopId());
+            shopQualificationPo.setShop(shopPo);
+            shopQualificationPo.setAuditStatus(Objects
+                    .equals(shopPo.getQualificationAuditStatus(), AuditStatus.NORMAL.getValue()) ?
+                    AuditStatus.NORMAL_AND_HAS_NEW_UPDATE_WAIT_FOR_AUDIT.getValue() :
+                    AuditStatus.WAIT_FOR_AUDIT.getValue());
+        }
+
+        shopQualificationPo.setBusinessLicence(reqVo.getBusinessLicence());
+        shopQualificationPo.setShopPhoto(reqVo.getShopPhoto());
+        shopQualificationPo.setLiquorSellLicence(reqVo.getLiquorSellLicence());
+        shopQualificationPo.setCorporateIdPhoto(reqVo.getCorporateIdPhoto());
+        shopQualificationPo.setCreateTime(DateUtil.now());
+        ShopQualificationPo savedShopQualificationPo =
+                shopQualificationRepository.save(shopQualificationPo);
+        publishEvent(new ShopQualificationUpdateEvent(this, savedShopQualificationPo));
     }
 
     @Override
     public ShopQualificationPo findLatestQualification(ShopDetailOrQualificationGetReqVo reqVo) throws CommonException {
-        return null;
+        UserRo user = userService.findUser(reqVo.getUserId());
+        validateShopOperateAuthority(user.getIsAdmin(), reqVo.getShopId(), user.getShopId());
+        return CollectionUtils.getFirstOrNull(
+                shopQualificationRepository.findByShopIdOrderByIdDesc(reqVo.getShopId()));
     }
 
     @Override
@@ -245,7 +298,41 @@ public class ShopServiceImpl extends CommonService  implements ShopService {
 
     @Override
     public void changeDeliveryAddress(ShopChangeDeliveryAddressReqVo reqVo) throws CommonException {
+        UserRo user = userService.findUser(reqVo.getUserId());
+        validateShopOperateAuthority(user.getIsAdmin(), reqVo.getShopId(), user.getShopId());
 
+        List<ShopDetailPo> unNormalDetails = shopDetailRepository
+                .findByShopIdAndAuditStatusInOrderByCreateTimeDesc(reqVo.getShopId(),
+                        newArrayList(AuditStatus.WAIT_FOR_AUDIT.getValue(),
+                                AuditStatus.NORMAL_AND_HAS_NEW_UPDATE_WAIT_FOR_AUDIT.getValue()));
+        if (CollectionUtils.isNotEmpty(unNormalDetails)) {
+            throw DepotnearbyExceptionFactory.Shop.SHOP_DETAIL_IS_AUDITING;
+        }
+        List<ShopDetailPo> normalDetails = shopDetailRepository
+                .findByShopIdAndAuditStatusOrderByCreateTimeDesc(reqVo.getShopId(),
+                        AuditStatus.NORMAL.getValue());
+        ShopDetailPo latestShopDetailPo = CollectionUtils.getFirstOrNull(normalDetails);
+        if (latestShopDetailPo == null) {
+            ShopPo shopPo = findShopPo(user.getShopId());
+            latestShopDetailPo = new ShopPoToShopDetailPo().apply(shopPo);
+        }
+        ShopDetailPo newShopDetailPo = new ShopDetailPo();
+        BeanUtils.copyProperties(latestShopDetailPo, newShopDetailPo);
+        newShopDetailPo.setId(null);
+        newShopDetailPo.setProvince(provinceRepository.findOne(reqVo.getProvinceId()));
+        newShopDetailPo.setCity(cityRepository.findOne(reqVo.getCityId()));
+        newShopDetailPo.setDistrict(districtRepository.findOne(reqVo.getDistrictId()));
+        newShopDetailPo.setDeliveryAddress(reqVo.getDeliveryAddress());
+        newShopDetailPo.setReason(reqVo.getReason());
+        ShopRo shop = findShop(reqVo.getShopId());
+        newShopDetailPo.setDeliveryName(shop.getDeliveryName());
+        newShopDetailPo.setDeliveryMobile(user.getMobile());
+        newShopDetailPo
+                .setAuditStatus(AuditStatus.NORMAL_AND_HAS_NEW_UPDATE_WAIT_FOR_AUDIT.getValue());
+        ShopDetailPo savedShopDetailPo = shopDetailRepository.save(newShopDetailPo);
+        //用户提交修改收货地址请求，会修改当前商铺的审核状态！！！
+        //        changeShopStatusToWaitForAudit(savedShopDetailPo);
+        publishEvent(new ShopDetailUpdateEvent(this, savedShopDetailPo));
     }
 
     @Override
@@ -255,7 +342,12 @@ public class ShopServiceImpl extends CommonService  implements ShopService {
 
     @Override
     public void changeDeliveryName(UserChangeDeliveryNameReqVo reqVo) throws CommonException {
-
+        UserRo user = userService.findUser(reqVo.getUserId());
+        validateShopOperateAuthority(user.getIsAdmin(), user.getShopId(), user.getShopId());
+        ShopPo shopPo = shopRepository.findOne(user.getShopId());
+        shopPo.setDeliveryName(reqVo.getDeliveryName());
+        shopRepository.updateDeliveryName(user.getShopId(), reqVo.getDeliveryName());
+        syncShopPoToRedis(shopPo);
     }
 
     @Override
