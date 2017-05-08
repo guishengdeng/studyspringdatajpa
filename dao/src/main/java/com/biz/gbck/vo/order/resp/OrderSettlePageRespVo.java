164d3203277f8bd0b722be05ee81789a6e6243d0
lei <helei_24@163.com>
2017-05-08 12:07:59 +0800

    add promotion

--- dao/src/main/java/com/biz/gbck/vo/order/resp/OrderSettlePageRespVo.java
+++ dao/src/main/java/com/biz/gbck/vo/order/resp/OrderSettlePageRespVo.java
@@ -30,7 +30,8 @@ public class OrderSettlePageRespVo extends BaseRequestVo {
      */
     private List<OrderItemRespVo> items;
 
-    //TODO 促销列表
+    // 促销列表
+    private List<OrderPromotionRespVo> promotions;
 
     // 可用优惠卷数量
     private Integer coupons = 0;
@@ -64,5 +65,93 @@ public class OrderSettlePageRespVo extends BaseRequestVo {
      * 买家收货地址
      */
     private String buyerAddress;
+
+    public Integer getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Integer orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Integer getFreight() {
+        return freight;
+    }
+
+    public void setFreight(Integer freight) {
+        this.freight = freight;
+    }
+
+    public List<OrderItemRespVo> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItemRespVo> items) {
+        this.items = items;
+    }
+
+    public Integer getCoupons() {
+        return coupons;
+    }
+
+    public void setCoupons(Integer coupons) {
+        this.coupons = coupons;
+    }
+
+    public List<Integer> getPaymentTypes() {
+        return paymentTypes;
+    }
+
+    public void setPaymentTypes(List<Integer> paymentTypes) {
+        this.paymentTypes = paymentTypes;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getBuyerMobile() {
+        return buyerMobile;
+    }
+
+    public void setBuyerMobile(String buyerMobile) {
+        this.buyerMobile = buyerMobile;
+    }
+
+    public String getBuyerAddress() {
+        return buyerAddress;
+    }
+
+    public void setBuyerAddress(String buyerAddress) {
+        this.buyerAddress = buyerAddress;
+    }
+
+    public List<OrderPromotionRespVo> getPromotions() {
+        return promotions;
+    }
+
+    public void setPromotions(List<OrderPromotionRespVo> promotions) {
+        this.promotions = promotions;
+    }
 }
 
