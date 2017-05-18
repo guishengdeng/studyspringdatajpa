96964f532312decd9fc30463af388a01cc8e95b4
lei <helei_24@163.com>
2017-05-18 10:40:20 +0800

    update stock

---
+++ biz-soa-feign/src/main/java/com/biz/soa/feign/client/stock/StockFeignClient.java
@@ -0,0 +1,56 @@
+package com.biz.soa.feign.client.stock;
+
+import com.biz.gbck.vo.stock.*;
+import com.biz.soa.feign.hystrix.order.OrderFeignClientHystrix;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ *
+ * @author lei
+ * @date 2017年05月16日
+ * @reviewer
+ * @see
+ */
+@FeignClient(name = "soa-order", fallback = OrderFeignClientHystrix.class)
+public interface StockFeignClient {
+
+    /**
+     * 获取库存
+     */
+    @RequestMapping("/soa/stock/app/getStock")
+    PartnerStockRespVO getStock(@RequestBody PartnerStockReqVO reqVo);
+
+    /**
+     * 获取批量库存
+     */
+    @RequestMapping("/soa/stock/app/getStocks")
+    List<PartnerStockRespVO> getStocks(@RequestBody PartnerStocksReqVO reqVo);
+
+    /**
+     * 添加购物车
+     */
+    @RequestMapping("/soa/stock/app/updateStocks")
+    void updateStocks(@RequestBody List<UpdatePartnerStockReqVO> reqVos);
+
+    /**
+     * 添加购物车
+     */
+    @RequestMapping("/soa/stock/app/updateLockStocks")
+    void orderUpdateLockStocks(@RequestBody List<UpdatePartnerLockStockReqVO> reqVos);
+
+    /**
+     * 添加购物车
+     */
+    @RequestMapping("/soa/stock/app/releaseLockStock")
+    void releaseLockStock() ;
+
+
+    @GetMapping(value = "/soa/stock/test")
+    String getTestString();
+
+}
