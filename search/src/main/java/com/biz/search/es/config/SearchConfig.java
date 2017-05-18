d314987b625bf54dc9be206ceba98390a3b34f18
david.liu <recalleverything@163.com>
2017-05-18 15:13:30 +0800

    append event publisher

--- search/src/main/java/com/biz/search/es/config/SearchConfig.java
+++ search/src/main/java/com/biz/search/es/config/SearchConfig.java
@@ -1,11 +1,16 @@
 package com.biz.search.es.config;
 
+import com.biz.core.event.BizEventMulticaster;
+import com.biz.core.event.BizEventPublisher;
+import com.biz.core.transaction.BizTransactionManager;
 import com.biz.service.IdService;
+import java.beans.PropertyVetoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 
 /**
  * Search Configuration
@@ -27,6 +32,23 @@ public class SearchConfig {
         String soaIdxZooNodePath = environment.getProperty("biz.zookeeper.soaIdx.path");
         String zookeeperUrl = environment.getProperty("biz.zookeeper.url");
         return new IdService(soaIdx, soaIdxZooNodePath, zookeeperUrl);
+    }
+
+    @Bean
+    public BizEventMulticaster bizEventMulticaster() {
+        return new BizEventMulticaster();
+    }
+
+    @Bean
+    public BizEventPublisher bizEventPublisher() {
+        return new BizEventPublisher();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(@Autowired BizEventPublisher bizEventPublisher) throws PropertyVetoException {
+        BizTransactionManager jpaTransactionManager = new BizTransactionManager();
+        jpaTransactionManager.setEventPublisher(bizEventPublisher);
+        return jpaTransactionManager;
     }
 
 }
