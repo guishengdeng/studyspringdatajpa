b88f18f4b98788f8c525c35f4bc149f7abcfa9dc
david.liu <recalleverything@163.com>
2017-05-20 17:03:13 +0800

    update ProductShowStatus enumeration value to correspond with sale status

--- dao/src/main/java/com/biz/gbck/enums/product/ProductShowStatus.java
+++ dao/src/main/java/com/biz/gbck/enums/product/ProductShowStatus.java
@@ -12,9 +12,8 @@ import com.biz.core.enums.EnumerableValue;
  */
 public enum ProductShowStatus implements EnumerableValue {
 
-    NORMAL(0), //正常
-    OFF_SALE(1), //下架
-    INSUFFICIENT_STOCK(2), //库存不足
+    NORMAL(10), //正常
+    OFF_SALE(0), //下架
     ;
 
     private int value;
