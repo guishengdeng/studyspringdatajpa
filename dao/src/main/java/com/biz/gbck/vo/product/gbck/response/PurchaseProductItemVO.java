c5e138e61577e902780060d1a6b1fb93bf734841
david.liu <recalleverything@163.com>
2017-05-20 15:32:43 +0800

    append minQuantity and maxQuantity to purchase product item vo

--- dao/src/main/java/com/biz/gbck/vo/product/gbck/response/PurchaseProductItemVO.java
+++ dao/src/main/java/com/biz/gbck/vo/product/gbck/response/PurchaseProductItemVO.java
@@ -11,14 +11,29 @@ import java.io.Serializable;
 public class PurchaseProductItemVO extends ProductAppListItemVo implements Serializable {
     private static final long serialVersionUID = 5228800915738935970L;
 
+    /**
+     * 商品显示状态
+     */
     private ProductShowStatus showStatus;
+
+    /**
+     * 商品最小起售数量
+     */
+    private Integer minQuantity;
+
+    /**
+     * 商品最大购买数量
+     */
+    private Integer maxQuantity;
 
     public PurchaseProductItemVO() {
     }
 
-    public PurchaseProductItemVO(ProductAppListItemVo itemVo, ProductShowStatus showStatus) {
+    public PurchaseProductItemVO(ProductAppListItemVo itemVo, ProductShowStatus showStatus, Integer minQuantity, Integer maxQuantity) {
         super(itemVo);
         this.showStatus = showStatus;
+        this.minQuantity = minQuantity;
+        this.maxQuantity = maxQuantity;
     }
 
     public ProductShowStatus getShowStatus() {
@@ -27,5 +42,21 @@ public class PurchaseProductItemVO extends ProductAppListItemVo implements Seria
 
     public void setShowStatus(ProductShowStatus showStatus) {
         this.showStatus = showStatus;
+    }
+
+    public Integer getMinQuantity() {
+        return minQuantity;
+    }
+
+    public void setMinQuantity(Integer minQuantity) {
+        this.minQuantity = minQuantity;
+    }
+
+    public Integer getMaxQuantity() {
+        return maxQuantity;
+    }
+
+    public void setMaxQuantity(Integer maxQuantity) {
+        this.maxQuantity = maxQuantity;
     }
 }
