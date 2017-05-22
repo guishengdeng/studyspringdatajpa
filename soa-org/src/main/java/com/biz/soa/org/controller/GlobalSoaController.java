dec6084388513d9c310aa0a8670032ae422101c4
dylan <dylan.hou@biz-united.com.cn>
2017-05-22 11:44:35 +0800

    update manage Notice

--- soa-org/src/main/java/com/biz/soa/org/controller/GlobalSoaController.java
+++ soa-org/src/main/java/com/biz/soa/org/controller/GlobalSoaController.java
@@ -2,9 +2,9 @@ package com.biz.soa.org.controller;
 
 import com.biz.core.ali.oss.BucketResVo;
 import com.biz.core.ali.oss.config.OssConfig;
-import com.biz.core.ali.oss.util.OssUtil;
 import com.biz.gbck.dao.redis.ro.upgrade.UpgradeRo;
 import com.biz.gbck.vo.config.AppConfigVo;
+import com.biz.gbck.vo.upgrade.AddUpgradeVo;
 import com.biz.soa.org.service.app.interfaces.AppSoaService;
 import com.biz.soa.org.service.upgrade.interfaces.UpgradeSoaService;
 import com.biz.support.web.handler.JSONResult;
@@ -15,11 +15,11 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,9 +27,9 @@ import static com.google.common.collect.Lists.newArrayList;
 
 @RestController
 @RequestMapping("soa/init")
-public class GlobalController extends BaseRestController{
+public class GlobalSoaController extends BaseRestController{
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalController.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalSoaController.class);
 
     @Autowired(required = false)
     private UpgradeSoaService upgradeSoaService;
@@ -38,6 +38,11 @@ public class GlobalController extends BaseRestController{
     private AppSoaService appSoaService;
 
 
+    /**
+     * 启动上报信息
+     * @param request
+     * @return
+     */
     @RequestMapping("/init")
     public JSONResult init(HttpServletRequest request) {
         logger.debug("Received soa-org /init/init GET request.");
@@ -49,7 +54,14 @@ public class GlobalController extends BaseRestController{
         return new JSONResult(result);
     }
 
-
+    /**
+     * 查询是否有可升级版本
+     * @param ver
+     * @param os
+     * @param partner
+     * @param request
+     * @return
+     */
     @RequestMapping("upgrade")
     public JSONResult upgrade(
             @RequestParam(value = "ver", required = true, defaultValue = "0") String ver,
@@ -65,6 +77,46 @@ public class GlobalController extends BaseRestController{
             return new JSONResult("need", false);
         }
     }
+
+    /**
+     * 根据型号查询升级配置集合
+     * @param os
+     * @return
+     */
+    @RequestMapping("findUpgradeByOs")
+    public List<UpgradeRo> findUpgradeByOs(@RequestParam("os") String os) {
+        return upgradeSoaService.findAll(os);
+    }
+
+    /**
+     * 根据id删除对应升级配置
+     * @param id
+     */
+    @RequestMapping("deleteUpgradeById")
+    public void deleteUpgradeById(@RequestParam("id") String id) {
+        upgradeSoaService.delete(id);
+    }
+
+    /**
+     * 添加升级配置
+     * @param upgrade
+     */
+    @RequestMapping("saveUpgrade")
+    public void saveUpgrade(@RequestBody AddUpgradeVo upgrade) {
+        upgradeSoaService.save(upgrade);
+    }
+
+    /**
+     * 判断给的升级版本号码是否存在
+     * @param version
+     * @param os
+     * @return
+     */
+    @RequestMapping("verifyVersion")
+    public boolean verifyVersion(@RequestParam("version") String version, @RequestParam("os") String os) {
+        return upgradeSoaService.verifyVersion(version,os);
+    }
+
 
     /**
      *获取隔壁仓库Oss上传bucket集合
--- soa-org/src/main/java/com/biz/soa/org/controller/InfoSoaController.java
+++ soa-org/src/main/java/com/biz/soa/org/controller/InfoSoaController.java
@@ -1,17 +1,20 @@
 package com.biz.soa.org.controller;
 
+import com.biz.gbck.common.exception.CommonException;
 import com.biz.gbck.dao.redis.ro.notice.NoticeRo;
 import com.biz.gbck.vo.info.ListNoticeAfterLastReqVo;
 import com.biz.gbck.vo.info.NoticeResVo;
 import com.biz.soa.org.service.notice.interfaces.NoticeSoaService;
 import com.biz.soa.org.transformer.NoticeRoToNoticeResVo;
 import com.biz.support.web.handler.JSONResult;
+import com.biz.vo.notify.NotifyVo;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,17 +29,18 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("soa/info") public class InfoController extends BaseRestController{
+@RequestMapping("soa/info")
+public class InfoSoaController extends BaseRestController{
 
 
-    private static Logger logger = LoggerFactory.getLogger(InfoController.class);
+    private static Logger logger = LoggerFactory.getLogger(InfoSoaController.class);
 
     @Autowired
     private NoticeSoaService noticeSoaService;
 
 
     /**
-     *消息中心
+     *rest给用户发送消息列表
      */
     @RequestMapping("/notices")
     public Object notices(@RequestBody ListNoticeAfterLastReqVo reqVo,
@@ -45,6 +49,17 @@ import java.util.List;
                 noticeSoaService.findUserNoticeAfter(Long.valueOf(reqVo.getUserId()), reqVo.getLastNoticeId());
         List<NoticeResVo> voList = Lists.transform(roList, new NoticeRoToNoticeResVo());
         return new JSONResult(voList);
+    }
+
+    /**
+     * manage给对应用户发送消息
+     * @param name 后台登录用户
+     * @param notifyVo 消息参数
+     */
+    @RequestMapping("/sendNotification")
+    public void sendNotification (@RequestParam("name") String name, @RequestBody NotifyVo notifyVo)
+            throws CommonException {
+         noticeSoaService.sendNotification(name,notifyVo);
     }
 
 
