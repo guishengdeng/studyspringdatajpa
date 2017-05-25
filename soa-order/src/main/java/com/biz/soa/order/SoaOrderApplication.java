a91964b21c4e575014cc3d40a802de3cc7c87da3
lei <helei_24@163.com>
2017-05-25 16:35:30 +0800

    fix configuration

--- soa-order/src/main/java/com/biz/soa/order/SoaOrderApplication.java
+++ soa-order/src/main/java/com/biz/soa/order/SoaOrderApplication.java
@@ -5,8 +5,10 @@ import com.biz.support.jpa.repository.CommonRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -22,8 +24,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.biz", repositoryFactoryBeanClass = CommonRepositoryFactoryBean.class)
 @EntityScan(basePackages = "com.biz")
 @EnableTransactionManagement
+@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrix
 public class SoaOrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(SoaOrderApplication.class, args);
