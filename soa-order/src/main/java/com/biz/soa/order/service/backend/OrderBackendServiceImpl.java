f200715664872c1e8114ddf2589263c712f373f0
lei <helei_24@163.com>
2017-05-25 15:19:40 +0800

    fix

--- soa-order/src/main/java/com/biz/soa/order/service/backend/OrderBackendServiceImpl.java
+++ soa-order/src/main/java/com/biz/soa/order/service/backend/OrderBackendServiceImpl.java
@@ -1,8 +1,14 @@
 package com.biz.soa.order.service.backend;
 
+import com.biz.gbck.dao.mysql.po.order.Order;
+import com.biz.gbck.vo.order.req.OrderQueryReqVo;
+import com.biz.gbck.vo.order.resp.OrderResponseVo;
 import com.biz.service.order.backend.OrderBackendService;
 import com.biz.soa.order.service.frontend.AbstractOrderService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 订单接口实现(后台)
@@ -15,4 +21,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderBackendServiceImpl extends AbstractOrderService implements OrderBackendService {
 
+    @Override
+    public List<Order> listOrder() {
+        return null;
+    }
+
+    @Override
+    public Page<Order> queryOrdersByCondition(OrderQueryReqVo vo) {
+        return null;
+    }
+
+    @Override
+    public Page<OrderResponseVo> pageOrder2PageRespVo(Page<Order> page) {
+        return null;
+    }
 }
