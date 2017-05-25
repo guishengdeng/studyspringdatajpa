67f29b1a2f427ba97891757e7250059aca09a5ab
lei <helei_24@163.com>
2017-05-25 18:49:04 +0800

    change cart vos

--- dao/src/main/java/com/biz/gbck/vo/cart/ShopCartItemAddReqVo.java
+++ dao/src/main/java/com/biz/gbck/vo/cart/ShopCartItemAddReqVo.java
@@ -1,6 +1,9 @@
 package com.biz.gbck.vo.cart;
 
+import com.biz.gbck.common.vo.CommonReqVoBindUserId;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * 添加购物车明细vo
@@ -8,7 +11,36 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * @author lei
  * @date 2017/01/12
  */
-public class ShopCartItemAddReqVo extends BaseShopCartReqVo {
+public class ShopCartItemAddReqVo extends CommonReqVoBindUserId {
+
+
+    /**
+     * 商品编码
+     */
+    @NotNull(message = "productId不能为空")
+    private String productId;
+
+    /**
+     * 数量
+     */
+    @NotNull(message = "quantity不能为空")
+    private Integer quantity = 1;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 
     @Override
     public String toString() {
--- dao/src/main/java/com/biz/gbck/vo/cart/ShopCartItemUpdateReqVo.java
+++ dao/src/main/java/com/biz/gbck/vo/cart/ShopCartItemUpdateReqVo.java
@@ -1,5 +1,9 @@
 package com.biz.gbck.vo.cart;
 
+import com.biz.gbck.common.vo.CommonReqVoBindUserId;
+
+import javax.validation.constraints.NotNull;
+
 /**
  * 更新购物车数量
  *
@@ -8,5 +12,33 @@ package com.biz.gbck.vo.cart;
  * @reviewer
  * @see
  */
-public class ShopCartItemUpdateReqVo extends BaseShopCartReqVo {
+public class ShopCartItemUpdateReqVo extends CommonReqVoBindUserId {
+
+    /**
+     * 商品编码
+     */
+    @NotNull(message = "productId不能为空")
+    private String productId;
+
+    /**
+     * 数量
+     */
+    @NotNull(message = "quantity不能为空")
+    private Integer quantity;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
