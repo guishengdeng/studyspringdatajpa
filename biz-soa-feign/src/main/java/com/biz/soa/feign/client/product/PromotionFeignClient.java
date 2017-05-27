b1376c3d0b2c7c9b00406752c1dd7ff9aa2bf645
lei <helei_24@163.com>
2017-05-27 13:51:32 +0800

    fix

--- biz-soa-feign/src/main/java/com/biz/soa/feign/client/product/PromotionFeignClient.java
+++ biz-soa-feign/src/main/java/com/biz/soa/feign/client/product/PromotionFeignClient.java
@@ -5,12 +5,11 @@ import com.biz.gbck.vo.product.promotion.OrderPromotionRespVO;
 import com.biz.gbck.vo.promotion.PromotionReqVo;
 import com.biz.gbck.vo.promotion.PromotionRespAppVo;
 import com.biz.gbck.vo.soa.MicroServiceResult;
-
-import java.util.List;
-
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by david-liu on 2017/04/27 10:12.
@@ -21,5 +20,6 @@ public interface PromotionFeignClient {
     @RequestMapping(value = "/product/promotion/order")
     MicroServiceResult<OrderPromotionRespVO> orderProductsPromotion(@RequestBody OrderPromotionReqVo reqVo);
 
+    @RequestMapping(value = "/product/promotion/promotionsForProductId")
 	List<PromotionRespAppVo> getUseablePromotionsForProductId(@RequestBody PromotionReqVo promotionReqVo);
 }
